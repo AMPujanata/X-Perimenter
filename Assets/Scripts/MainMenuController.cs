5c8264039ac7236e2ce609bcfc50d464adae18cb
AMPujanata <73583984+AMPujanata@users.noreply.github.com>
2020-11-04 21:28:29 +0700

    Main Menu and some Scene setup
    Added main menu functionally for both loading scenes and quitting the application. Additionally, added canvases that are specifically for adding background images that automatically scale with the camera.

---
+++ Assets/Scripts/MainMenuController.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenuController : MonoBehaviour
+{
+    public void LoadLevel(string levelName) //used when passing only the scene's name into the method
+    {
+        //add any extra setup before load scene
+        SceneManager.LoadScene(levelName);
+    }
+
+    public void LoadLevel(Scene level) //used when passing a whole Scene into the method
+    {
+        SceneManager.LoadScene(level.name);
+    }
+
+    public void QuitGame()
+    {
+        //add any extra cleanup here
+        Application.Quit();
+    }
+}
